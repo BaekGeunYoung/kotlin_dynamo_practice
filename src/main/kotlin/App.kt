204d3248db64d76e7a6089d363227e7bc410808a
BaekGeunYoung <dvmflstm@naver.com>
2019-12-27 00:54:44 +0900

    trying connection & create table request

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,30 @@
+import com.amazonaws.AmazonServiceException
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.amazonaws.services.dynamodbv2.model.*
+
+
+fun main() {
+    val tableName = "test_table"
+    val request: CreateTableRequest = CreateTableRequest()
+        .withAttributeDefinitions(
+            AttributeDefinition(
+                "Name", ScalarAttributeType.S
+            )
+        )
+        .withKeySchema(KeySchemaElement("Name", KeyType.HASH))
+        .withProvisionedThroughput(ProvisionedThroughput(10, 10))
+        .withTableName(tableName)
+
+    val ddb: AmazonDynamoDB = AmazonDynamoDBClientBuilder.standard()
+        .withRegion(Regions.AP_NORTHEAST_2)
+        .build()
+
+    try {
+        val result = ddb.createTable(request)
+        println(result.tableDescription.tableName)
+    } catch (e: AmazonServiceException) {
+        println(e.errorMessage)
+    }
+}
